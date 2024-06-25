53b82e240781cf34fc07e12c574b30833a44cc41
nerdfavour <favourackonu13@gmail.com>
2024-06-25 14:24:16 +0000

    Add MyCards and Statistics to NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,17 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './home-screen/HomeScreen';
 import SettingsScreen from './settings-screen/SettingsScreen';
+import MyCardsScreen from './my-cards-screen/MyCardsScreen';
+import StatisticsScreen from './statistics-screen/StatisticsScreen';
+
 const Tab = createBottomTabNavigator();
 
 export default function NavBar() {
   return (
     <Tab.Navigator>
       <Tab.Screen name = "Home" component = {HomeScreen} />
+      <Tab.Screen name = "My Cards" component = {MyCardsScreen} />
+      <Tab.Screen name = "Statistics" component = {StatisticsScreen} />
       <Tab.Screen name = "Settings" component = {SettingsScreen} />
     </Tab.Navigator>
   );
