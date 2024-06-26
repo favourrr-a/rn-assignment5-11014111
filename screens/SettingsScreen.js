45f58e51a8c0b40a8fcdfc7754cc87c302424732
nerdfavour <favourackonu13@gmail.com>
2024-06-26 23:33:31 +0000

    Add categories to settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { useContext } from 'react';
 import { AppContext } from '../store/context';
 
@@ -16,6 +16,38 @@ export default function SettingsScreen(){
     return (
         <View style = {appThemedStyle.container}> 
             <View style = {settingsScreenThemedStyle.container}>
+                <View style = {settingsScreenThemedStyle.settingsHeadingContainer}>
+                    <Text style = {settingsScreenThemedStyle.settingsHeading}>
+                        Settings
+                    </Text>
+                </View>
+                <View style = {settingsScreenThemedStyle.categoriesContainer}>
+                    <View style = {settingsScreenThemedStyle.settingsCategoryContainer}>
+                        <Text style = {settingsScreenThemedStyle.settingsCategory}> 
+                            Language
+                        </Text>
+                    </View>
+                    <View style = {settingsScreenThemedStyle.settingsCategoryContainer}>
+                        <Text style = {settingsScreenThemedStyle.settingsCategory}> 
+                            My profile
+                        </Text>
+                    </View>
+                    <View style = {settingsScreenThemedStyle.settingsCategoryContainer}>
+                        <Text style = {settingsScreenThemedStyle.settingsCategory}> 
+                            Contact us
+                        </Text>
+                    </View>
+                    <View style = {settingsScreenThemedStyle.settingsCategoryContainer}>
+                        <Text style = {settingsScreenThemedStyle.settingsCategory}> 
+                            Change password
+                        </Text>
+                    </View>
+                    <View style = {settingsScreenThemedStyle.settingsCategoryContainer}>
+                        <Text style = {settingsScreenThemedStyle.settingsCategory}> 
+                            Privacy policy
+                        </Text>
+                    </View>
+                </View>
                 <ThemeSetting />
             </View>
         </View>
--- styles/settings-screen-styles/settingsScreenStyles.js
+++ styles/settings-screen-styles/settingsScreenStyles.js
@@ -6,13 +6,56 @@ export const settingsScreenStyles = (theme) => {
             marginTop: 30,
             marginLeft: 10,
             marginRight: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            width: '90%',
+            marginTop: 30,
         },
+        settingsHeadingContainer: {
+            alignItems: 'center',
+        },
+        settingsHeading: {
+            fontSize: 24,
+        },
+        categoriesContainer: {
+            marginTop: 0,
+        },
+        settingsCategoryContainer: {
+            height: 50,
+            width: '100%',
+            borderBottomWidth: 2,
+            
+            position: 'relative'
+        },
+        settingsCategory: {
+            position: 'absolute',
+            bottom: 0,
+            fontSize: 20,
+        }
     }
     if(theme === 'light'){
         return StyleSheet.create({
             container: {
                 ...commonStyles.container,
             },
+            settingsHeadingContainer: {
+                ...commonStyles.settingsHeadingContainer,
+            },
+            settingsHeading: {
+                ...commonStyles.settingsHeading,
+                color: '#1e1e2d',
+            },
+            categoriesContainer: {
+                ...commonStyles.categoriesContainer,
+            },
+            settingsCategoryContainer: {
+                ...commonStyles.settingsCategoryContainer,
+                borderBottomColor: '#f4f4f4',
+            },
+            settingsCategory: {
+                ...commonStyles.settingsCategory,
+                color: '#1e1e2d',
+            }
         });
     }
     else{
@@ -20,6 +63,24 @@ export const settingsScreenStyles = (theme) => {
             container: {
                 ...commonStyles.container,
             },
+            settingsHeadingContainer: {
+                ...commonStyles.settingsHeadingContainer,
+            },
+            settingsHeading: {
+                ...commonStyles.settingsHeading,
+                color: 'white',
+            },
+            categoriesContainer: {
+                ...commonStyles.categoriesContainer,
+            },
+            settingsCategoryContainer: {
+                ...commonStyles.settingsCategoryContainer,
+                borderBottomColor: '#232533',
+            },
+            settingsCategory: {
+                ...commonStyles.settingsCategory,
+                color: 'white',
+            }
         });
     }
 }
